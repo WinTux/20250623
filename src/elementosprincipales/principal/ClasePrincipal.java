007de40b856914671ea207cb014a3f4ca94fa52a
rusok <rusok@localhost>
2025-06-25 18:27:39 -0400

    Primer commit

---
+++ src/elementosprincipales/principal/ClasePrincipal.java
@@ -0,0 +1,44 @@
+package elementosprincipales.principal;
+
+public class ClasePrincipal { // Clase, tipo
+
+	public static void main(String[] args) {
+		// Lenguaje de alto nivel
+		// Lenguaje fuertemente tipado
+		// Lenguaje de tipado estático
+		
+		int a = 10, b = 23;
+		int c;
+		
+		System.out.print("Pepe");
+		System.out.println("Perales");
+		System.out.println("28");
+		
+		System.out.println("La suma es: "+(a+b));
+		System.out.println("La resta es: "+(a-b));
+		System.out.println("La multi es: "+(a*b));
+		System.out.println("La division es: "+(a/b));
+		System.out.println("******** Con decimales ********");
+		double aa = 10, bb = 23;
+		System.out.println("La suma es: "+(aa+bb));
+		System.out.println("La resta es: "+(aa-bb));
+		System.out.println("La multi es: "+(aa*bb));
+		System.out.println("La division es: "+(aa/bb));
+		
+		float aaa = 10, bbb = 23;
+		System.out.println("La suma es: "+(aaa+bbb));
+		System.out.println("La resta es: "+(aaa-bbb));
+		System.out.println("La multi es: "+(aaa*bbb));
+		System.out.println("La division es: "+(aaa/bbb));
+		
+		
+		
+	}
+	
+	public static void arrancar() {
+		
+	}
+	public static void hackearLaNASA() { // Camel case
+		
+	}
+}
